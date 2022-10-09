7850ac7afa1ed6f94c1d448ee8a579f0539db5ad
Vatsal Vora <vatsaliit30@gmail.com>
2022-10-09 22:52:58 +0530

    Update spotify.py

--- api/spotify.py
+++ api/spotify.py
@@ -22,7 +22,7 @@ FALLBACK_THEME = "spotify.html.j2"
 REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token"
 NOW_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-playing"
 RECENTLY_PLAYING_URL = (
-    "https://api.spotify.com/v1/me/player/recently-played?limit=10"
+    "https://api.spotify.com/v1/me/player/recently-played?limit=1"
 )
 
 app = Flask(__name__)
